dcaa52abb5166b609867875fc6ea995ff99ab030
Chris Earle <chris.earle@elasticsearch.com>
2015-03-30 19:47:42 -0400

    Adding test fo IndicesAdminClientr getMappings

--- src/test/groovy/org/elasticsearch/groovy/client/IndicesAdminClientExtensionsActionTests.groovy
+++ src/test/groovy/org/elasticsearch/groovy/client/IndicesAdminClientExtensionsActionTests.groovy
@@ -18,9 +18,11 @@
  */
 package org.elasticsearch.groovy.client
 
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse
 import org.elasticsearch.action.admin.indices.refresh.RefreshResponse
 import org.elasticsearch.action.search.SearchResponse
 import org.elasticsearch.client.IndicesAdminClient
+import org.elasticsearch.cluster.metadata.MappingMetaData
 
 import org.junit.Before
 import org.junit.Test
@@ -76,5 +78,31 @@ class IndicesAdminClientExtensionsActionTests extends AbstractClientTests {
 
         assert searchResponse.hits.totalHits == 1
         assert searchResponse.hits.hits[0].id == docId
+    }
+
+    @Test
+    void testGetMappingRequest() {
+        // index a document to guarantee that a mapping exists
+        indexDoc(indexName, typeName) {
+            name = "needle"
+        }
+
+        // ensure that the mapping exists
+        // refresh the index to guarantee searchability
+        RefreshResponse refreshResponse = indicesAdminClient.refresh {
+            indices indexName
+        }.actionGet()
+
+        assert refreshResponse.failedShards == 0
+
+        GetMappingsResponse response = indicesAdminClient.getMappings {
+            indices indexName
+        }.actionGet()
+
+        // extra the mapping for the current index/type
+        MappingMetaData mappingMetaData = response.mappings.get(indexName).get(typeName)
+
+        // ensure that we properly mapped the 'name' field to a string
+        assert mappingMetaData.sourceAsMap['properties']['name']['type'] == 'string'
     }
 }
